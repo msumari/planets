0e80c91df5baaec6c7863b6d3c3d541a231307ae
msumari <suleykhery@gmail.com>
2022-06-20 23:50:04 +0300

    Added POST  /launches test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,14 +3,40 @@ const app = require("../../app");
 
 describe("Test GET /launches", () => {
   test("Should respond with 200 success", async () => {
-    const response = await request(app).get("/launches").expect(200);
+    const response = await request(app)
+      .get("/launches")
+      .expect(200)
+      .expect("Content-Type", /json/);
   });
 });
 
 describe("Test POST /launches", () => {
-  test("Should respond with 201 success", () => {
-    const response = 201;
-    expect(response.statusCode).toBe(201);
+  const completeLaunch = {
+    mission: "USSR",
+    rocket: "Falcon 1",
+    launchDate: "January 1, 2020",
+    target: "kepler-186 f",
+  };
+
+  const incompleteLaunch = {
+    mission: "USSR",
+    rocket: "Falcon 1",
+    target: "kepler-186 f",
+  };
+
+  test("Should respond with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunch)
+      .expect(201)
+      .expect("Content-Type", /json/);
+
+    const requestDate = new Date(completeLaunch.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(requestDate).toBe(responseDate);
+
+    expect(response.body).toMatchObject(incompleteLaunch);
   });
   test("Should catch missing launch data", () => {
     const response = 400;
